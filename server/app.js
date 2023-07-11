a4fd8f4ee14676104bca06ebf48fae6312e48eff
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-07-10 21:10:09 -0300

    fixes cors allowed origins

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5500",
+    origin: "https://web-rtc-jet.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
